8b454994ef3214e06531e3b680275ae7d8c1a336
llayz46 <layzer@outlook.fr>
2024-08-28 22:24:07 +0200

    adding disabled on button to decrement quantity if quantity equals to 1

--- resources/views/livewire/cart/cart.blade.php
+++ resources/views/livewire/cart/cart.blade.php
@@ -75,7 +75,7 @@
                                                                 Qty
                                                             </p>
 
-                                                            <button wire:click="decrement({{ $item->id }})" class="ml-2 mr-1 rounded hover:bg-gray-200 hover:text-gray-700">
+                                                            <button wire:click="decrement({{ $item->id }})" @disabled($item->quantity === 1) class="ml-2 mr-1 rounded hover:bg-gray-200 hover:text-gray-700">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
                                                                 </svg>
