4de6d059978dbedc26db9353e6913bd4db2191fb
llayz46 <layzer@outlook.fr>
2024-09-13 22:29:56 +0200

    filters products animation

--- resources/views/livewire/product/products.blade.php
+++ resources/views/livewire/product/products.blade.php
@@ -36,21 +36,17 @@
 
                 <!-- Filters -->
                 <form class="mt-4">
-                    <div class="border-t border-gray-200 pb-4 pt-4">
+                    <div class="border-t border-gray-200 pb-4 pt-4" x-data="{ isCapacityOpen: false }">
                         <fieldset>
                             <legend class="w-full px-2">
                                 <!-- Expand/collapse section button -->
-                                <button type="button"
+                                <button type="button" @click="isCapacityOpen = !isCapacityOpen"
                                         class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500"
-                                        aria-controls="filter-section-0" aria-expanded="false">
-                                    <span class="text-sm font-medium text-gray-900">Color</span>
+                                        aria-controls="filter-section-1" aria-expanded="false">
+                                    <span class="text-sm font-medium text-gray-900">Capacity</span>
                                     <span class="ml-6 flex h-7 items-center">
-                      <!--
-                        Expand/collapse icon, toggle classes based on section open state.
 
-                        Open: "-rotate-180", Closed: "rotate-0"
-                      -->
-                      <svg class="rotate-0 h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor"
+                      <svg :class="isCapacityOpen ? '-rotate-180' : 'rotate-0'" class="h-5 w-5 transform transition-transform duration-300" viewBox="0 0 20 20" fill="currentColor"
                            aria-hidden="true">
                         <path fill-rule="evenodd"
                               d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
@@ -58,68 +54,16 @@
                       </svg>
                     </span>
                         </button>
-                        </legend>
-                            <div class="px-4 pb-2 pt-4" id="filter-section-0">
-                                <div class="space-y-6">
-                                    <div class="flex items-center">
-                                        <input id="color-0-mobile" name="color[]" value="white" type="checkbox"
-                                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                        <label for="color-0-mobile" class="ml-3 text-sm text-gray-500">White</label>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <input id="color-1-mobile" name="color[]" value="beige" type="checkbox"
-                                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                        <label for="color-1-mobile" class="ml-3 text-sm text-gray-500">Beige</label>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <input id="color-2-mobile" name="color[]" value="blue" type="checkbox"
-                                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                        <label for="color-2-mobile" class="ml-3 text-sm text-gray-500">Blue</label>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <input id="color-3-mobile" name="color[]" value="brown" type="checkbox"
-                                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                        <label for="color-3-mobile" class="ml-3 text-sm text-gray-500">Brown</label>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <input id="color-4-mobile" name="color[]" value="green" type="checkbox"
-                                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                        <label for="color-4-mobile" class="ml-3 text-sm text-gray-500">Green</label>
-                                    </div>
-                                    <div class="flex items-center">
-                                        <input id="color-5-mobile" name="color[]" value="purple" type="checkbox"
-                                               class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                        <label for="color-5-mobile" class="ml-3 text-sm text-gray-500">Purple</label>
-                                    </div>
-                                </div>
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div class="border-t border-gray-200 pb-4 pt-4">
-                        <fieldset>
-                            <legend class="w-full px-2">
-                                <!-- Expand/collapse section button -->
-                                <button type="button"
-                                        class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500"
-                                        aria-controls="filter-section-1" aria-expanded="false">
-                                    <span class="text-sm font-medium text-gray-900">Category</span>
-                                    <span class="ml-6 flex h-7 items-center">
-                      <!--
-                        Expand/collapse icon, toggle classes based on section open state.
-
-                        Open: "-rotate-180", Closed: "rotate-0"
-                      -->
-                      <svg class="rotate-0 h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor"
-                           aria-hidden="true">
-                        <path fill-rule="evenodd"
-                              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                              clip-rule="evenodd"/>
-                      </svg>
-                    </span>
-                                </button>
                             </legend>
-                            <div class="px-4 pb-2 pt-4" id="filter-section-1">
-                                <div class="space-y-6">
+                            <div class="px-4 pb-2 pt-4">
+                                <div class="space-y-6"
+                                     x-show="isCapacityOpen" x-cloak
+                                     x-transition:enter="transition ease-out duration-300"
+                                     x-transition:enter-start="opacity-0 transform -translate-y-10"
+                                     x-transition:enter-end="opacity-100 transform translate-y-0"
+                                     x-transition:leave="transition ease-in duration-300"
+                                     x-transition:leave-start="opacity-100 transform translate-y-0"
+                                     x-transition:leave-end="opacity-0 transform -translate-y-10">
                                     <div class="flex items-center">
                                         <input id="category-0-mobile" name="category[]" value="new-arrivals"
                                                type="checkbox"
@@ -157,11 +101,11 @@
                             </div>
                         </fieldset>
                     </div>
-                    <div class="border-t border-gray-200 pb-4 pt-4">
+                    <div class="border-t border-gray-200 pb-4 pt-4" x-data="{ isSizeOpen: false }">
                         <fieldset>
                             <legend class="w-full px-2">
                                 <!-- Expand/collapse section button -->
-                                <button type="button"
+                                <button type="button" @click="isSizeOpen = !isSizeOpen"
                                         class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500"
                                         aria-controls="filter-section-2" aria-expanded="false">
                                     <span class="text-sm font-medium text-gray-900">Sizes</span>
@@ -171,17 +115,24 @@
 
                         Open: "-rotate-180", Closed: "rotate-0"
                       -->
-                      <svg class="rotate-0 h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor"
+                      <svg :class="isSizeOpen ? '-rotate-180' : 'rotate-0'" class="h-5 w-5 transform transition-transform duration-300" viewBox="0 0 20 20" fill="currentColor"
                            aria-hidden="true">
                         <path fill-rule="evenodd"
                               d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
                               clip-rule="evenodd"/>
                       </svg>
                     </span>
-                                </button>
+                        </button>
                             </legend>
-                            <div class="px-4 pb-2 pt-4" id="filter-section-2">
-                                <div class="space-y-6">
+                            <div class="px-4 pb-2 pt-4">
+                                <div class="space-y-6"
+                                     x-show="isSizeOpen" x-cloak
+                                     x-transition:enter="transition ease-out duration-300"
+                                     x-transition:enter-start="opacity-0 transform -translate-y-10"
+                                     x-transition:enter-end="opacity-100 transform translate-y-0"
+                                     x-transition:leave="transition ease-in duration-300"
+                                     x-transition:leave-start="opacity-100 transform translate-y-0"
+                                     x-transition:leave-end="opacity-0 transform -translate-y-10">
                                     <div class="flex items-center">
                                         <input id="sizes-0-mobile" name="sizes[]" value="xs" type="checkbox"
                                                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
