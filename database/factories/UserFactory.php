6529f1596a13b39fe9bd947438bb6241bb629b9e
llayz46 <layzer@outlook.fr>
2024-09-16 17:40:15 +0200

    displaying last 3 comments for product show page

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,19 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $colorsArray = [
+            'FFB6C1', 'FF69B4', 'FF1493', 'DB7093', 'FFC0CB',
+            'FF6347', 'FF4500', 'FFD700', 'FFA500', 'FF8C00',
+            'FFFFE0', 'FFFACD', 'FAFAD2', 'FFEFD5', 'FFE4B5',
+            'FFDAB9', 'EEE8AA', 'F0E68C', 'BDB76B', 'E6E6FA',
+            'D8BFD8', 'DDA0DD', 'DA70D6', 'EE82EE', 'FF00FF'
+        ];
+
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'avatar' => 'https://placeholderjs.com/750x750&text=' . urlencode(fake()->firstName()) . '&background=_' . fake()->hexColor(),
+            'avatar' => 'https://placeholderjs.com/750x750&text=' . urlencode(fake()->firstName()) . '&background=_' . $this->faker->randomElement($colorsArray),
             'role' => 'user',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('testtest'),
